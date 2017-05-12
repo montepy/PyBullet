89d6f681a49715e2bb6151050343c82310f2572b
montepy <epywang42@gmail.com>
2017-05-12 09:23:47 -0400

    input implementation(fully featured)

--- PythonTestingInitiative/PythonTestingInitiative/PythonTestingInitiative.py
+++ PythonTestingInitiative/PythonTestingInitiative/PythonTestingInitiative.py
@@ -8,16 +8,16 @@ pygame.display.set_caption("First")
 background = pygame.Surface(screen.get_size())
 background = background.convert()
 class Controllable(pygame.Rect):
-    def __init__(self,left,top,width,height):
-        pygame.Rect.__init__(self,left,top,width,height,hvelocity = 0,vvelocity = 0)
-        hvelocity = 0#edit this
-        vvelocity = 0
+    def __init__(self,left,top,width,height,hvelocity = 0,vvelocity = 0):
+        pygame.Rect.__init__(self,left,top,width,height)
+        self.hvelocity = hvelocity
+        self.vvelocity = vvelocity
 
     def update(self):
-        dis.x += hvelocity
-        dis.y += vvelocity
+        dis.x += self.hvelocity
+        dis.y += self.vvelocity
 
-dis = Controllable(25,25,25,25)
+dis = Controllable(25,25,50,50)
 #dis = pygame.Rect(25,25,25,25)
 done = False
 clock = pygame.time.Clock()
@@ -31,13 +31,13 @@ while not done:
             if event.key == pygame.K_ESCAPE:
                 done = True
             if event.key == pygame.K_UP:
-                dis.vvelocity = -2
+                dis.vvelocity = -4
             elif event.key == pygame.K_RIGHT:
-                 dis.hvelocity = 2
+                 dis.hvelocity = 4
             elif event.key == pygame.K_DOWN:
-                dis.vvelocity = 2
+                dis.vvelocity = 4
             elif event.key == pygame.K_LEFT:
-                dis.hvelocity = -2
+                dis.hvelocity = -4
 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
@@ -55,7 +55,5 @@ while not done:
     screen.blit(background,(0,0))
 
     pygame.display.flip()
-    if dis.right>=400:
-        done = True
 
 pygame.quit()
