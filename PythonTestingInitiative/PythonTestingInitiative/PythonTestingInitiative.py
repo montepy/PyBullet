f77f1bb49d433ec4e750fb6aa59d998f7334d9b1
montepy <epywang42@gmail.com>
2017-05-12 08:12:52 -0400

    Detailed testing

--- PythonTestingInitiative/PythonTestingInitiative/PythonTestingInitiative.py
+++ PythonTestingInitiative/PythonTestingInitiative/PythonTestingInitiative.py
@@ -7,7 +7,19 @@ screen = pygame.display.set_mode((640,480))
 pygame.display.set_caption("First")
 background = pygame.Surface(screen.get_size())
 background = background.convert()
-background.fill((250,250,250))
-screen.blit(background,(0,0))
-pygame.display.flip()
+dis = pygame.Rect(25,25,25,25)
+done = False
+clock = pygame.time.Clock()
 
+while not done:
+    clock.tick(30)
+    dis.right += 1;
+    background.fill((250,250,250))
+    pygame.draw.rect(background,pygame.color.Color(50,200,50),dis)
+    screen.blit(background,(0,0))
+
+    pygame.display.flip()
+    if dis.right>=100:
+        done = True
+
+pygame.quit()
